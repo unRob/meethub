87eaf4c6b9f8d79b093e850d963d38176d91735f
rob <rob@partidosurrealista.com>
2015-10-06 03:22:14 -0500

    haciendo el pedo de meetup a ojo de buen cubero

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var http = require('http');
 var fs = require('fs');
 var Evento = require('./lib/evento.js');
 var _ = require('lodash');
+var qs = require('querystring');
+var url = require('url');
 
 var cfg = JSON.parse(fs.readFileSync('config.json'));
 
 // var content = 'https://api.github.com/repos/javascriptmx/chelajs/contents/eventos';
 var meetup = require('meetup-api')(cfg.meetup.credentials);
 var webhook = require('github-webhook-handler')(cfg.github.hook);
-var github = require('octonode').client();
-var ghrepo = github.repo(cfg.github.repo);
+var github = require('octonode');
 Evento.configure(cfg.meetup.defaults);
+
+var auth_url = github.auth.config(cfg.github.credentials).login(['repo']);
+
 
 var json = function(message, code) {
   if (code){
@@ -30,9 +34,32 @@ var starts_with = function(string) {
 
 http.createServer(function (req, res) {
   webhook(req, res, function (err) {
-    if (!err) res.json({"error": "Not found"}, 404);
+    if (!err) {
+      var uri = url.parse(req.url);
+
+      if (uri.pathname.indexOf('/auth/github') === 0 && !cfg.github.access_token) {
+        switch (uri.pathname) {
+
+          case '/auth/github':
+            res.writeHead(302, {'Content-Type': 'text/plain', 'Location': auth_url});
+            res.end('Redirecting to ' + auth_url);
+          break;
+
+          case '/auth/github/callback':
+            var values = qs.parse(uri.query);
+            github.auth.login(values.code, function (err, token) {
+              res.json(token);
+            });
+          break;
+
+        }
+      } else {
+        res.json({"error": "Not found"}, 404);
+      }
+    }
   });
 }).listen(cfg.port);
+
 
 webhook.on('error', function (err) {
   console.error('Error:', err.message);
@@ -48,14 +75,13 @@ webhook.on('push', function (event) {
   var of_events = starts_with(folder);
 
   pl.commits.forEach(function(commit){
-    if (commit.message.indexOf('x-meethub-commit') == -1) {
+    if (commit.committer.email !== 'meethub@rob.mx') {
       var files = commit.modified.concat(commit.added);
-      console.log(files, files.filter(of_events));
       events = _.union(events, files.filter(of_events));
     }
   });
 
-  console.log(events);
+  var ghrepo = github.client(cfg.github.access_token).repo(cfg.github.repo);
 
   events.forEach(function(file){
     ghrepo.contents(file, function(err, res){
@@ -70,33 +96,38 @@ webhook.on('push', function (event) {
 
       var action = evento.id ? 'editEvent' : 'postEvent';
 
-      console.log(action, evento);
-      // meetup[action](evento.to_meetup(), function(err, resp){
-      //   if (err) {
-      //     console.log(err, resp);
-      //   } else {
-      //     if (action == 'postEvent') {
-      //       evento.id = resp.id;
-      //       evento.contents = evento.contents.replace('<!--\n', 'id: '+evento.id);
+      meetup[action](evento.to_meetup(), function(err, resp){
+        if (err) {
+          console.log(err, resp);
+        } else {
+          if (action == 'postEvent') {
+            evento.id = resp.id;
+            evento.contents = evento.contents.replace('<!--\n', 'id: '+evento.id);
 
-      //       var url = ['repos', cfg.repo, 'contents', res.path].join('/');
-      //       var opts = {
-      //         path: res.path,
-      //         message: 'x-meethub-commit',
-      //         content: new Buffer(evento.contents).toString('base64'),
-      //         sha: res.sha,
-      //         committer: {
-      //           name: "Meethub Bot",
-      //           email: "meethub@rob.mx"
-      //         }
-      //       };
+            var url = ['repos', cfg.repo, 'contents', res.path].join('/');
+            var opts = {
+              path: res.path,
+              message: 'x-meethub-commit',
+              content: new Buffer(evento.contents).toString('base64'),
+              sha: res.sha,
+              committer: {
+                name: "Meethub Bot",
+                email: "meethub@rob.mx"
+              }
+            };
 
-      //       github.put(url, opts, function(err, s, b, h){
-
-      //       });
-      //     }
-      //   }
-      // });
+            github.put(url, opts, function(err, s, b, h){
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Event #"+evento.id+" created");
+              }
+            });
+          } else {
+            console.log("Event #"+evento.id+" updated");
+          }
+        }
+      });
     });
   });
 
